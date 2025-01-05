69ecc25d34c5b4dbcff8960eb607af9a105957b6
aki030854 <133839992+aki030854@users.noreply.github.com>
2025-01-05 18:59:27 +0900

    Add files via upload
    提出する

---
+++ kadai_004/event.js
@@ -0,0 +1,20 @@
+
+    // loadイベント
+    
+    $(window).on('load', function () {
+        console.log('loadイベントが発生しました');
+        
+
+
+    });
+
+
+
+
+    // scrollイベント
+    $(window).on('scroll', function () {
+
+        console.log('scrollイベントが発生しました');
+    });
+
+
